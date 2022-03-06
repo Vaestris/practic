0e2a19de301356032928aa6a94b1343b564d8a2c
Vaestris <70700749+Vaestris@users.noreply.github.com>
2022-03-07 01:37:00 +0300

    DBmodel_0.51

--- DBmodel.cs
+++ DBmodel.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 
 namespace internet_shop_test2
 {
     public class ProgramContext : DbContext
     {
-        public DbSet<customer> customers { get; set; }
-        public DbSet<order> orders { get; set; }
-        public DbSet<product> products { get; set; }
-        public DbSet<delivery> deliveries { get; set; }
+       // public DbSet<customer> Customers { get; set; }
+       // public DbSet<order> Orders { get; set; }
+        public DbSet<product> Products { get; set; }
+       // public DbSet<delivery> Deliveries { get; set; }
         public ProgramContext()
         {
             Database.EnsureCreated();
         }
 
         //private const string connectionstring = @"Server=.\MSSQLSERVER;Database=internet_shop_test;Trusted_Connection=True;";
-        private const string connectionstring = @"Server=(localdb)\mssqllocaldb;Database=internet_sop_test;Trusted_Connection=True;";
+        private const string connectionstring = @"Server=DESKTOP-8HTVICI;Database=internet_sop_test;Trusted_Connection=True;";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -25,10 +26,13 @@ namespace internet_shop_test2
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<customer>().HasKey(u => u.phone_number);
-            modelBuilder.Entity<order>().HasKey(u => u.order_number);
-            modelBuilder.Entity<product>().HasKey(u => u.product_number);
-            modelBuilder.Entity<delivery>().HasNoKey();
+            
+           // modelBuilder.Entity<customer>().HasKey(u => u.phone_number);
+           // modelBuilder.Entity<order>().HasKey(u => u.id_order);
+           // modelBuilder.Entity<order>().HasOne(p => p.customer).WithMany(b => b.Orders).HasForeignKey(p => p.phone_number);
+            modelBuilder.Entity<product>().HasKey(u => u.id);
+           // modelBuilder.Entity<delivery>().HasNoKey();
+            //modelBuilder.Entity<Department>().Property(t => t.Name).IsRequired();
             //modelBuilder.Entity<customer>().HasNoKey();
             //modelBuilder.Entity<order>().HasNoKey();
             //modelBuilder.Entity<product>().HasNoKey();
@@ -39,28 +43,34 @@ namespace internet_shop_test2
     }
 
 
-    public class customer
+   /* public class customer
     {
         public int phone_number { get; set; }
-        public char fname { get; set; }
-        public char lname { get; set; }
-        public char company { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string company { get; set; }
+
+        public List<order> Orders { get; set; }
 
     }
     public class order
     {
-        public int phone_number { get; set; }
-        public int order_number { get; set; }
+        
+        public int id_order { get; set; }
         public int product_number { get; set; }
+        public int phone_number { get; set; }//внещний ключ
 
-    }
+        public customer customer { get; set; }//навигационные свойство 
+
+    }*/
     public class product
     {
+        public int id { get; set; }
         public int product_number { get; set; }
         public int existence { get; set; }
         public string name { get; set; }
     }
-    public class delivery
+  /*  public class delivery
     {
         public int order_number { get; set; }
         public bool shipment { get; set; }
@@ -68,5 +78,5 @@ namespace internet_shop_test2
         public bool delivered { get; set; }
         public DateTime time { get; set; }
 
-    }
+    }*/
 }
--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@ namespace internet_shop_test2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             using (var db=new ProgramContext())
             {
-                db.products.Add(new product { product_number = 1, existence = 1, name = "штука" });
+               // db.Customers.Add(new customer {phone_number = 1, fname = "name", lname = "Lname", company = "company" });
+               // db.Orders.Add(new order {phone_number = 1, id_order = 1, product_number = 1 });
+                db.Products.Add(new product {product_number = 1, existence = 1, name = "shtuka" });
+               // db.deliveries.Add(new delivery { order_number = 1, shipment = true, issued = true, delivered  = true, time = "";});
                 db.SaveChanges();
             }
         }
