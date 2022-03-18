47ffa35282ef67e482065911384ef0fdbe024cbd
Vaestris <70700749+Vaestris@users.noreply.github.com>
2022-03-18 14:37:47 +0300

    Add files via upload

---
+++ Internet_shop_test/Internet_shop_test/Repository/Dataproducts.cs
@@ -0,0 +1,29 @@
+﻿using Internet_shop_test.interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Internet_shop_test.Repository
+{
+    public class Dataproducts : IGetProducts
+    {
+        private readonly ProgramContext ProgramContext;
+        public Dataproducts(ProgramContext ProgramContext)
+        {
+            this.ProgramContext = ProgramContext;
+        }
+
+        //public IEnumerable<product> AllCategories => throw new NotImplementedException();
+
+        public IEnumerable<product> AllProducts => ProgramContext.Products.Include(c => c.existence);
+
+
+
+        public product getproductid(int id) => ProgramContext.Products.FirstOrDefault(p => p.id == id);
+
+    }
+}
