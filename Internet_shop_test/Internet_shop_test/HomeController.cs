1f7dda5d8ddc8177492a0eaf8f8619e68335a6fd
Vaestris <70700749+Vaestris@users.noreply.github.com>
2022-03-18 14:33:15 +0300

    Add files via upload

---
+++ Internet_shop_test/Internet_shop_test/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Internet_shop_test.interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Internet_shop_test
+{
+    public class HomeController : Controller
+    {
+        private readonly IGetProducts _ProductsInterface;
+        
+        public HomeController(IGetProducts iproducts)
+        {
+            _ProductsInterface = iproducts;
+        }
+
+       
+
+        public ViewResult ProductList()
+        {
+            var p_roducts = _ProductsInterface.AllProducts;
+            return View(p_roducts);
+        }
+        
+    }
+
+    
+}
---
+++ Internet_shop_test/Internet_shop_test/interfaces/Customers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Internet_shop_test.interfaces
+{
+    interface Customers
+    {
+        IEnumerable<customer> AllCategories { set; }
+    }
+}
