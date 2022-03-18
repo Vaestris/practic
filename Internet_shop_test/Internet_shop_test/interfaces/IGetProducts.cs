f1f6a11e67553d6eb4467bcb0c5c483405182185
Vaestris <70700749+Vaestris@users.noreply.github.com>
2022-03-18 14:35:13 +0300

    Add files via upload

---
+++ Internet_shop_test/Internet_shop_test/interfaces/IGetProducts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Internet_shop_test.interfaces
+{
+    public interface IGetProducts
+    {
+        IEnumerable<product> AllProducts { get; }
+        product getproductid(int id);
+    }
+}
